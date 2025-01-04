fb0e3bacf53f79bcbee5bec6dbf1b9958b3488e8
vishalumralia <vishalumralia@gmail.com>
2025-01-04 16:36:11 +0530

    data_types_in_python

---
+++ Basics/data_types_in_python/data_types.py
@@ -0,0 +1,23 @@
+#  int, float, string, list and set
+x = 50
+x = 60.5
+x = "Hello World"
+x = ["Hello", "Welcome"]
+x = ("Hello", "Welcome")
+
+
+x = "Hello World"	# str	
+x = 5	# int	
+x = 10.5	# float	
+x = 1j	# complex	
+x = ["apple", "banana", "cherry"]	# list	
+x = ("apple", "banana", "cherry")	# tuple	
+x = range(6)	# range	
+x = {"name" : "abc", "age" : 20}	# dict	
+x = {"apple", "banana", "cherry"}	# set	
+x = frozenset({"apple", "banana", "cherry"})	# frozenset	
+x = True	# bool	
+x = b"Hello"	# bytes	
+x = bytearray(5)	# bytearray	
+x = memoryview(bytes(5))	# memoryview	
+x = None	# NoneType
